d41b1f548e013617f4002ce9a3ed42db454848b8
Nathan Forester <Nathan.Forester@estio.co.uk>
2023-11-21 13:49:40 +0000

    adding vagrantfile and ansible folder

---
+++ Vagrantfile.rb
@@ -0,0 +1,27 @@
+# Vagrant.configure("2") do |config|
+#   config.vm.define "app" do |app|
+#     app.vm.box = "bento/ubuntu-20.04"
+#     config.ssh.insert_key = false
+#     app.vm.network :private_network, ip: "192.168.33.12"
+#     app.vm.hostname = "app"
+#     app.hostsupdater.aliases = ["app"]
+#  end
+
+  config.vm.define "deploy" do |deploy|
+    deploy.vm.box = "bento/ubuntu-20.04"
+    config.ssh.insert_key = false
+    deploy.vm.network :private_network, ip: "192.168.33.11"
+    deploy.vm.hostname = "app"
+    deploy.hostsupdater.aliases = ["app"]
+    deploy.vm.synced_folder "ansible", "/home/ubuntu/ansible"
+  end
+
+  config.vm.define "ansible" do |ansible|
+    ansible.vm.box = "bento/ubuntu-20.04"
+    ansible.vm.network :private_network, ip: "192.168.33.10"
+    ansible.vm.hostname = "ansible"
+    ansible.hostsupdater.aliases = ["ansible"]
+    ansible.vm.provision "shell", path: "provision.sh"
+    ansible.vm.synced_folder "ansible", "/home/ubuntu/ansible"
+  end
+end
